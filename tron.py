c5a76f4013fd05751f020b18b759a29cef7e6460
aragant <tlemaire1610@gmail.com>
2022-10-10 10:09:50 +0200

    Revert "ptite line de fred"
    This reverts commit cd5cc4ba8f1bddcb9579d623418efe9e1f080426.

--- tron.py
+++ tron.py
@@ -141,17 +141,7 @@ class MazeWindow(arcade.Window):
 
 class Agent:
     def __init__(self, env, alpha = 1, gamma = 0.6, cooling_rate = 0.999):
-        self.__qtable = {}
-        for state in env.states:
-            self.__qtable[state] = {}
-            for action in ACTIONS:
-                self.__qtable[state][action] = 0.0
-        
-        self.__env = env
-        self.__alpha = alpha
-        self.__gamma = gamma
-        self.__history = []
-        self.__cooling_rate = cooling_rate
+        pass
 
 
     def best_action(self):
